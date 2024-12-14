b78781216dd1c5b59fc7153a431bc844b203ebc2
huanghao <huanghao@youxin.cloud>
2024-12-14 10:08:02 +0800

    Base64

---
+++ src/libs/base64url.test.ts
@@ -0,0 +1,12 @@
+import crypto from "node:crypto";
+import { expect, test } from "vitest";
+import { decodeBase64Url, encodeBase64Url } from "./base64url";
+
+test("decodeBase64Url", async () => {
+	const secret = crypto.randomBytes(32);
+	const encoded = encodeBase64Url(secret);
+	expect(encoded.length).toBe(43);
+
+	const decoded = decodeBase64Url(encoded);
+	expect(secret.length).toBe(32);
+});
--- src/libs/base64url.ts
+++ src/libs/base64url.ts
@@ -1,3 +1,6 @@
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
 export function decodeBase64(encoded: string): Uint8Array {
 	const binary = atob(encoded);
 	const bytes = new Uint8Array(binary.length);
@@ -7,12 +10,38 @@ export function decodeBase64(encoded: string): Uint8Array {
 	return bytes;
 }
 
-export function decodeBase64Url(input: string) {
+export function decodeBase64Url(input: Uint8Array | string) {
 	let encoded = input;
+	if (encoded instanceof Uint8Array) {
+		encoded = decoder.decode(encoded);
+	}
 	encoded = encoded.replace(/-/g, "+").replace(/_/g, "/").replace(/\s/g, "");
 	try {
 		return decodeBase64(encoded);
 	} catch {
 		throw new TypeError("The input to be decoded is not correctly encoded.");
 	}
+}
+
+export function encodeBase64(input: Uint8Array | string) {
+	let unencoded = input;
+	if (typeof unencoded === "string") {
+		unencoded = encoder.encode(unencoded);
+	}
+	const CHUNK_SIZE = 0x8000;
+	const arr = [];
+	for (let i = 0; i < unencoded.length; i += CHUNK_SIZE) {
+		arr.push(
+			// @ts-expect-error
+			String.fromCharCode.apply(null, unencoded.subarray(i, i + CHUNK_SIZE)),
+		);
+	}
+	return btoa(arr.join(""));
+}
+
+export function encodeBase64Url(input: Uint8Array | string) {
+	return encodeBase64(input)
+		.replace(/=/g, "")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
 }
